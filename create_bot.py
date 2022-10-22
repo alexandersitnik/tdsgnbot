5cac54bd2f623f5cbcf54f29fc121aee81076dc2
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-10-22 21:23:00 +0700

    initial commit

--- create_bot.py
+++ create_bot.py
@@ -1,5 +1,8 @@
 from aiogram import Bot, Dispatcher
 from data.config import TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
---
+++ database.py
@@ -0,0 +1,49 @@
+from email import message
+import sqlite3
+from unicodedata import name
+
+currentId = None
+
+try:
+    db = sqlite3.connect('./data/tdsgnBotBase.db')
+    c = db.cursor()
+except:
+    pass
+
+def get_members():
+    c.execute("SELECT * FROM members")
+    return c.fetchall()
+def format_members():
+    members = get_members()
+    for i in members:
+        members_string = 'ID: ' + str(i[0]) + '\n' \
+            + 'Name: ' + i[1] + '\n' \
+            + 'Department: ' + i[2] + '\n' \
+            + 'Grade: ' + i[3] + '\n' \
+            + 'Birthday: ' + i[4] + '\n' \
+            + 'Employer: ' + i[5] + '\n'
+    return members_string
+print(format_members())
+
+def get_current_id():
+    currentId =  len(get_members()) + 1
+    return currentId
+
+def set_member(id, name, department, grade, birthday, employer):
+    c.execute("INSERT INTO members VALUES (?, ?, ?, ?, ?, ?)", (id, name, department, grade, birthday, employer))
+    db.commit()
+get_current_id()
+
+async def sql_add_deception(state):
+    async with state.proxy() as data:
+        c.execute("INSERT INTO deseption VALUES (?, ?, ?)", tuple(data.values()))
+        db.commit()
+
+def sql_get_last_deception():
+    last_deception = c.execute("SELECT * FROM deseption").fetchall()
+    for el in last_deception:
+        last_deception_text = 'Имя лжеца: ' + str(el[0]) + '\n' \
+            + 'Дата обмана: ' + el[1] + '\n' \
+            + 'Текст обмана: ' + el[2] + '\n'
+        return last_deception_text
+print(sql_get_last_deception())
