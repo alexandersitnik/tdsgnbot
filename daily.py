e0e8960e20a35cdfab1315d78970a7a586fb82c2
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-11-25 03:05:33 +0700

    formated text

--- daily.py
+++ daily.py
@@ -21,9 +21,9 @@ async def dailyReport():
     if distant_today != []:
         for el in distant_today:
             distant_today_list += '— ' + str(el[0]) + '\n'
-        daily_report += "Сегодня удалёнка у: \n\n" + str(distant_today_list) + '\n'
+        daily_report += "*Сегодня удалёнка у:* \n\n" + str(distant_today_list) + '\n'
     else:
-        daily_report += "Сегодня удалёнки ни у кого нет" + '\n\n'
+        daily_report += "*Сегодня удалёнки ни у кого нет*" + '\n\n'
 
     sick_today = c.execute("SELECT members.Name, sick.isActive FROM members INNER JOIN sick ON members.ID = sick.MemberID WHERE sick.SickDate = ?", (today,)).fetchall()
     if sick_today != []:
@@ -33,18 +33,18 @@ async def dailyReport():
                 sick_today_str += '— ' + i[0] + ' - работает из дома\n'
             else:
                 sick_today_str += '— ' + i[0] + ' - не работает\n'
-        daily_report += "\nСегодня болеют:\n\n" + sick_today_str + '\n'
+        daily_report += "\n*Сегодня болеют:*\n\n" + sick_today_str + '\n'
     else:
-        daily_report += "\nСегодня никто не болеет\n\n"
+        daily_report += "\n*Сегодня никто не болеет*\n\n"
 
     vacations = c.execute("SELECT * FROM vacation WHERE StartVacationDay <= ? AND EndVacationDay >= ?", (datetime.now(), datetime.now())).fetchall()
     todayVacationsAnswer = ''
     if len(vacations) == 0:
-        daily_report += "\nСегодня в отпуске никого"
+        daily_report += "\n*Сегодня в отпуске никого*"
     else:
         for vacation in vacations:
             memberName = c.execute("SELECT Name FROM members WHERE ID = ?", (vacation[0],)).fetchone()[0]
             todayVacationsAnswer += '— ' + memberName + '\n'
-            daily_report += "\nСегодня в отпуске:\n\n" + todayVacationsAnswer
-    await bot.send_message(-235938403, daily_report)
-    await bot.send_message(-1001723462410, daily_report)
+            daily_report += "\n*Сегодня в отпуске*:\n\n" + todayVacationsAnswer
+    await bot.send_message(-235938403, daily_report, parse_mode='Markdown')
+    await bot.send_message(-1001723462410, daily_report, parse_mode='Markdown')
