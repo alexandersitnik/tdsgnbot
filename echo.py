bb1d9252cac0ce9c2cd5df077bc4661ad84b3f7b
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-11-30 13:47:44 +0700

    new cocksize

--- echo.py
+++ echo.py
@@ -1,22 +1,26 @@
 from create_bot import dp, bot
+import random
 import hashlib
 from aiogram.types import InlineQuery, \
     InputTextMessageContent, InlineQueryResultArticle
-
+epithets = ['членохера', 'пипки', 'достоинства', 'хрена', 'шишки','болта','сморчка','убивца','писюна','агрегата','елдака']
 async def inline_echo(inline_query: InlineQuery):
-    print('sdfdsfdsf')
+    print('отработала инлайн функция')
     # id affects both preview and content,
     # so it has to be unique for each result
     # (Unique identifier for this result, 1-64 Bytes)
     # you can set your unique id's
     # but for example i'll generate it based on text because I know, that
     # only text will be passed in this example
-    text = inline_query.query or 'echo'
+    cocksize_len = random.randint(1, 40)
+    epithet = random.choice(epithets)
+    text = 'Твой размер ' + epithet + ' ' + str(cocksize_len) + ' см'
+    # text = str(cocksize_len) or 'Не могу разглядеть'
     input_content = InputTextMessageContent(text)
     result_id: str = hashlib.md5(text.encode()).hexdigest()
     item = InlineQueryResultArticle(
         id=result_id,
-        title=f'Result {text!r}',
+        title=f'Ну давай, покажи свой размер {epithet}',
         input_message_content=input_content,
     )
     # don't forget to set cache_time=1 for testing (default is 300s or 5m)
