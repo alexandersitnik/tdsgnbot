f7809e26d24c036023c61e7cde46161fd577851d
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-12-07 18:26:09 +0700

    delete chatblock

--- geshtimer.py
+++ geshtimer.py
@@ -11,16 +11,5 @@ async def geshtimer(message: types.Message):
     geshSeconds = geashFate.split(' ')[2].split(':')[2].split('.')[0]
     await message.answer('Стартуем в Геш через: \n' + geshDays + ' д. ' + geshHours + ' ч. ' + geshMinutes + ' мин. ' + geshSeconds + ' сек.')
 
-async def blockchat(message: types.Message):
-    # chatFate = str(datetime.now() - blockstart)
-    # print(chatFate)
-    # chatDays = chatFate.split(' ')[0]
-    # chatHours = chatFate.split(' ')[2].split(':')[0]
-    # chatMinutes = chatFate.split(' ')[2].split(':')[1]
-    # chatSeconds = chatFate.split(' ')[2].split(':')[2].split('.')[0]
-    # await message.answer('Чатоблокада длится уже: \n' + chatDays + ' д. ' + chatHours + ' ч. ' + chatMinutes + ' мин. ' + chatSeconds + ' сек.')
-    await message.answer('Чатоблокада окончена.\n Пока что...')
-
 def register_handlers_gesh(dp: Dispatcher):
     dp.register_message_handler(geshtimer, commands=['geshtime'])
-    dp.register_message_handler(blockchat, commands=['blocktime'])
