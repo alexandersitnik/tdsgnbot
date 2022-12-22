7bdb8ecfede050e2eff5d756cc715f08c56b55dc
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-12-22 19:20:28 +0700

    gesh

--- geshtimer.py
+++ geshtimer.py
@@ -6,14 +6,12 @@ from globals import geshstart, blockstart
 async def geshtimer(message: types.Message):
     geashFate = str(geshstart - datetime.now())
     # geshDays = geashFate.split(' ')[0]
-    geshHours = geashFate.split(' ')[0].split(':')[0]
-    geshMinutes = geashFate.split(' ')[0].split(':')[1]
-    geshSeconds = geashFate.split(' ')[0].split(':')[2].split('.')[0]
-    if int(geshHours) < 0 and int(geshMinutes) < 0 and int(geshSeconds) < 0:
-        await message.answer('Время Геша настало!')
-        await message.answer_sticker(r'CAACAgIAAxkBAAEG8ShjpDnOlUS9-8h45anKXsf8LZCbGwACnwYAAulVBRh_hSfBhR2zUywE')
-    else:
-        await message.answer('Стартуем в Геш через: \n' + geshHours + ' ч. ' + geshMinutes + ' мин. ' + geshSeconds + ' сек.')
+    # geshHours = geashFate.split(' ')[0].split(':')[0]
+    # geshMinutes = geashFate.split(' ')[0].split(':')[1]
+    # geshSeconds = geashFate.split(' ')[0].split(':')[2].split('.')[0]
+    await message.answer('Время Геша настало!')
+    await message.answer_sticker(r'CAACAgIAAxkBAAEG8ShjpDnOlUS9-8h45anKXsf8LZCbGwACnwYAAulVBRh_hSfBhR2zUywE')
+    # await message.answer('Стартуем в Геш через: \n' + geshHours + ' ч. ' + geshMinutes + ' мин. ' + geshSeconds + ' сек.')
 
 def register_handlers_gesh(dp: Dispatcher):
     dp.register_message_handler(geshtimer, commands=['geshtime'])
