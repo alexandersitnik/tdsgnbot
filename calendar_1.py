fc77a1efc14bd5d74706db840ebe870655703e06
Sitnik Alexander <sitnik.ksl@gmail.com>
2023-02-10 10:37:54 +0700

    пофиксил пагинацию в календаре

--- calendar_1.py
+++ calendar_1.py
@@ -16,21 +16,18 @@ def create_calendar(year: int, month: int):
                 markup.insert(InlineKeyboardButton(text=' ', callback_data='pass'))
             else:
                 markup.insert(InlineKeyboardButton(text=str(day), callback_data=f'day-{day}'))
-    markup.row(InlineKeyboardButton(text='<', callback_data='previous-month'),
-               InlineKeyboardButton(text='>', callback_data='next-month'))
+    markup.row(InlineKeyboardButton(text='<<', callback_data='previous-month'),
+               InlineKeyboardButton(text='>>', callback_data='next-month'))
     return markup
 
-# @dp.callback_query_handler(lambda c: c.data and c.data.startswith('day-'))
 async def process_calendar_selection(callback_query: types.CallbackQuery):
     selected, day = callback_query.data.split('-')
     await bot.send_message(callback_query.from_user.id, f'Вы выбрали {day} число')
     return day
 
-# @dp.callback_query_handler(lambda c: c.data == 'pass')
 async def process_pass_button(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, 'Дата не выбрана')
 
-# @dp.callback_query_handler(lambda c: c.data == 'previous-month')
 async def process_previous_month_button(callback_query: types.CallbackQuery):
     global tempdate
     previous_month = tempdate - timedelta(days=30)
@@ -39,16 +36,17 @@ async def process_previous_month_button(callback_query: types.CallbackQuery):
     markup = create_calendar(previous_month.year, previous_month.month)
     await bot.edit_message_text(f"Дата: {client_date}", callback_query.from_user.id, callback_query.message.message_id, reply_markup=markup)
 
-# @dp.callback_query_handler(lambda c: c.data == 'next-month')
 async def process_next_month_button(callback_query: types.CallbackQuery):
     global tempdate
     next_month = tempdate + timedelta(days=30)
     tempdate = next_month
     client_date = str(month_list[tempdate.month]) + ' ' + str(tempdate.year)
     markup = create_calendar(next_month.year, next_month.month)
-    await bot.edit_message_text(f"Дата: {client_date}", callback_query.from_user.id, callback_query.message.message_id, reply_markup=markup)
+    await bot.edit_message_text(f"{client_date}", callback_query.from_user.id, callback_query.message.message_id, reply_markup=markup)
 
 async def process_calendar_command(message: types.Message):
+    global tempdate
+    tempdate = datetime.now()
     current = datetime.now()
     client_date = month_list[current.month] + ' ' + str(current.year)
     markup = create_calendar(current.year, current.month)
