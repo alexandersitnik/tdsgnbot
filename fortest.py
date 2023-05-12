629c225be159663f5f7a9b49852f7be8124cf181
Sitnik Alexander <sitnik.ksl@gmail.com>
2023-05-12 10:34:51 +0700

    predskaz

--- fortest.py
+++ fortest.py
@@ -39,7 +39,7 @@ async def get_new_year_fortune(message: types.Message):
     if sender_member is None:
         log.info(f"In get_new_year_fortune got unknown telegram_id. From user: {message.from_user.first_name}")
     sender_name = sender_member if sender_member is not None else message.from_user.first_name
-    fortune_template = f"{sender_name}, сегодня тебя ждёт"
+    fortune_template = f"{sender_name}, вот тебе заебательское предсказание:"
     generated_fortune = None
     reply_message = await message.reply("Составляю предсказние на неделю...📝")
     aiohttp_session = ClientSession(trust_env=True)
